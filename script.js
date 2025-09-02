033306fa9edbc8c03f117731024ac14bac110d0a
jawwad9 <jawwadmuhammad514@gmail.com>
2025-09-02 09:14:00 -0700

    Api data render card

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-const searchBox = document.querySelector('.searchBox')
-const searchBtn = document.querySelector('.searchBtn')
-const recipeContainer = document.querySelector('.recipe-container')
+const searchBox = document.querySelector('.searchBox');
+const searchBtn = document.querySelector('.searchBtn');
+const recipeContainer = document.querySelector('.recipe-container');
 
 
 
 
 const fetchRecipe = async (query) => {
-    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+    recipeContainer.innerHTML = "<h2>Fetching Recipe</h2>";
+    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
     const respones = await data.json();
-    console.log(respones);
-    
+    // console.log(respones.meals[0]);
+
+    recipeContainer.innerHTML = "";
+    respones.meals.forEach( meal => {
+        // console.log(meal);
+        const recipeDiv = document.createElement('div')
+        recipeDiv.classList.add('recipe')
+        recipeDiv.innerHTML = `
+        <img src="${meal.strMealThumb}">
+        <h3>${meal.strMeal}</h3>
+        <p><span>${meal.strArea}</span>Dish</p>
+        <p>Belong to <span>${meal.strCategory} </span>Category</p>
+        `
+        const botton = document.createElement('button');
+        botton.textContent = "View Recire";
+        recipeDiv.appendChild(botton);
+
+        recipeContainer.appendChild(recipeDiv);
+    })
     
 }
 
